bc0cb0c52ef56585d0bc3bab2517fb2fc3e8d767
Hernan Mendez <hernan21112001@gmail.com>
2017-10-03 19:55:44 -0300

    Updated, Updater, (with dates)

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const renderToStaticMarkup = require('react-dom/server').renderToStaticMarkup;
 const React = require('react')
 let mongoose = require('mongoose')
-mongoose.connect(require('./mongosettings'))
+mongoose.connect(require('./mongosettings'), {
+  useMongoClient: true
+})
 //requires and connection to the database
 
 execSync("grunt babel");
@@ -15,7 +17,7 @@ let names = fs.readdirSync("./compiled")
   .map(name => name.substr(0, name.length - 3))
 
 components = components.map((component, index) =>
-{ return { name: names[index], component: component } })
+{ return { name: names[index], component: component, date: (new Date).getTime() } })
 
 //deleting the 'compiled' folder
 for (let filename of fs.readdirSync('compiled')) {
@@ -25,7 +27,7 @@ fs.rmdirSync('compiled')
 ///////////////////////////////
 
 //updating the database
-let storySchema = mongoose.Schema({ name: String, component: String })
+let storySchema = mongoose.Schema({ name: String, component: String, date: Number })
 let storyModel = mongoose.model('stories', storySchema)
 
   for (let story in components) {
@@ -47,6 +49,7 @@ let storyModel = mongoose.model('stories', storySchema)
           if(error) return console.error('something went wrong with '+ components[story].name + ': ' + error)
           console.log('Done '+components[story].name) 
           if(story == components.length-1) process.exit(0)
+            
         })
       }
     })
