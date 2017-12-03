ef510aad1e7b69f1197785aeb527b5d641e2f62a
Rai Lee <thisisrailee@gmail.com>
2017-12-03 13:33:30 -0800

    add more stories

---
+++ stories/dante.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Story = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/dante.jpg" alt="" />
+    <h4 className="story-title"><em>Dante, CTO at Corisaude</em></h4>
+    <p><em>This week we got a chance to interview Dante Nolasco. He is the CTO of Corisaude, a startup working to improve elderly health. Dante’s startup was created at a Hackathon, where his team won $30,000 and a contract with the second largest health insurance company in Brazil. Here is the interview:</em></p>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I am 18 years old, I live in Brazil, and I like to create and learn new things. My family is involved in fashion retail and I was involved in it when I was young. In 2014, I moved to another city for high school and to attend a business program. At that time, I watched startups closely which sparked my interest in graphic design and filmmaking. In 2015, I attended an international event in NYC and I received the Best Web Design Award for a company created on a school project. It was a really simple project created on Wix but winning the award pushed me along the trajectory of programming. Since then, I started getting more and more involved with programming and web design. I had my first job when I was 17 as a Front-End React Developer at an entrepreneurial education startup.”</p>
+    <p><strong>How did you start coding?</strong></p>
+    <p>“I was always very interested in numbers and computers, especially Excel. When I was 12, I took an online HTML course from Codecademy. Since then, I have been involved with technology, especially web development. For a long time, programming was just a hobby and I was more interested on learning concepts instead of putting them into practice. It wasn’t until I was 16 that I saw programming as a real opportunity and something that I could pursue as a career. I decided to try out building a career in coding. Since then, I have been learning a lot from online courses offered through Coursera, Udemy, and Udacity.”</p>
+    <p><strong>What inspired you to want to become a software engineer?</strong></p>
+    <p>“I think what caught my attention is how challenging it is. It's different from being hard, but it involves a lot of creativity and problem solving, and I always enjoyed things like LEGO and puzzles. I think it has some things in common, and that's what inspired me. For the last two years, I’ve become more motivated as I started getting more involved with startups and bigger projects.”</p>
+    <p><strong>Can you tell us a little about Corisaude?</strong></p>
+    <p>“Sure! Cori is a startup company focused on improving elderly health. It was created in 2016 during a Startup Weekend event at my school. Since then, we’ve developed the product and business model by participating in acceleration programs. Cori helps fragile patients, who need full-time monitoring by a caregiver. Cori helps monitor their daily routines, behavior, eating habits and other variables in order to prevent hospitalization. There are only four people involved in the company and I am the only one with programming skills. It has been a really interesting approach, being both an entrepreneur and software engineer. This experience really changed the way I think about coding. I had to start learning not only about databases and frameworks, but also about geriatrics and elderly patients' challenges of using smartphones. I also learned concepts like statistical analysis for big data, AI, and Scrum - things I consider essential for every software engineer.”</p>
+    <p><strong>What is some advice that you would give to people who are trying to become software engineers?</strong></p>
+    <p>“First you should find an area you enjoy. In my case, it was web design. Then, you dive deep into learning the concepts, usage and frameworks. Keep pushing yourself to always create bigger projects and ensure you really have that knowledge. The learning part is pretty hard, because you have to manage it by yourself. Once you feel confident about your skills, start looking for jobs. My personal advice is to start in a small company, especially if you have never worked before. This gives you more responsibility and makes you grow professionally.”</p>
+    <p><strong>Did anyone help you in becoming a software engineer? If so, how did they help you?</strong></p>
+    <p>“Yes, my family have always supported me and encouraged me to always face bigger challenges. I am also very thankful for my teachers and colleagues from high school, who recognized my skills and helped me network.”</p>
+    <p><strong>Do you have any mentors? How have they impacted you on your journey?</strong></p>
+    <p>“Unfortunately I don’t have any mentors, and this is something I regret and would like to change. I really think that having a mentor makes things easier.”</p>
+    <p><strong>Any last thoughts/advice?</strong></p>
+    <p>“Make sure you enjoy what you are doing, and always have in mind two things: learning and networking. With skill and opportunity, you will reach your goals. Keep pushing to be the best.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default Story;
---
+++ stories/stephen.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Story = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/stephen.jpg" alt="" />
+    <h4 className="story-title"><em>Stephen, Software Engineer at Marketo</em></h4>
+    <p><em>This week we got a chance to interview Stephen Leung. He is a Software Engineer at Marketo. Stephen is passionate about programing and has gone through Hack Reactor to become a Software Engineer. Here is the interview:</em></p>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I graduated from UC Davis with an Applied Physics Degree and I decided to become a teacher. Both of my parents were teachers so it was inevitable that I would give teaching a try. I even got a master’s degree in education. I taught public high school math for four years. I liked it but I didn’t like it enough to do it for the rest of my life. I considered programming because I grew up playing computer games; not relevant, but still. This was in 2011, there weren’t really any bootcamps, so I decided to try to get into the tech field through working with hardware. But working on servers wasn’t helping me write any software, and I wasn’t anywhere near my goal of being a software engineer. I started out teaching myself a little bit of coding, and when I found FreeCodeCamp, I used it to learn HTML, CSS, and JavaScript. I still didn’t feel ready applying for jobs so, in 2016, I quit my job as an Information Systems Technician to pursue programming full-time. I stumbled across Hack Reactor. I applied to their program, but was rejected. The week after that was terrible, because I felt that I was ready and that they didn’t know how much I knew. I was pretty bummed; it felt like getting rejected from your dream school. But the upside was that I could reapply in three months, so I devoted a lot of my time to studying basic JavaScript. I got in my second time around.”</p>
+    <p><strong>What did you learn in Hack Reactor?</strong></p>
+    <p>“They didn’t focus too much on HTML and CSS, but really dived deep into JavaScript. We worked with CS fundamentals like data structures and algorithm design, D3, AngularJS, Backbone.js, React, Node.js, Express, MongoDB, MySQL, Git.  That’s all that I can remember for now but there might have been more frameworks and libraries. We then had collaborative projects where we built several applications from scratch - anything we wanted. Looking back, we made some amazing apps.”</p>
+    <p><strong>How did you like the program?</strong></p>
+    <p>“It was good. I liked working and learning with other people who wanted to be software engineers. It prepared me well. They had job coaches; we would work on projects to showcase our programming knowledge, and we had a lot of mock interviews.”</p>
+    <p><strong>What was the job search process like?</strong></p>
+    <p>“It takes time. It took me four months after graduation to find a job. By the time that I received my offer, I had submitted around 300 applications. Of those 300 applications, I only received 4 phone screens, 2 onsite interviews and 1 job offer. The job offer was good so I accepted it. It was stressful because there’s so much uncertainty. But looking back, I didn’t need to be as stressed. It’s a process, and you have to persist with continuously learning and persevere through all the rejections.”</p>
+    <p><strong>What was the job interview process like with the company that you received an offer with?</strong></p>
+    <p>“The initial part was a 30-minute phone screen. Then, it was an algorithm challenge through Skype. The final part was a four-hour on-site interview with multiple white boarding exercises from several engineers.”</p>
+    <p><strong>Would you have done anything differently looking backwards?</strong></p>
+    <p>“Not necessarily, because Hack Reactor helped me learn things much quicker than what I would have learned on my own. I do wish that I found people trying to become software engineers prior to going to Hack Reactor because I feel like I could have grasped certain concepts much quicker if someone was there to point me toward the right direction. That’s a reason why I go to FreeCodeCamp meetups now, because I want to be the person that I needed when I was becoming a software engineer.”</p>
+    <p><strong>What is some advice for people who are trying to become software engineers?</strong></p>
+    <p>“It’s important to figure out what you are passionate about. If you like designing and making things look good, maybe you’ll want to focus on being a Front-End Developer. If you’re a very logical person, you might think about being a Back-End Developer. It’s important to understand what it is that you like to do because you don’t want to hate your job. You want to do something that you love.”</p>
+    <p><strong>Do you have any last thoughts/advice?</strong></p>
+    <p>“Becoming a Software Engineer is a long journey, so just keep at it. Nobody should have to go through the journey alone, so reach out and meet with people going through, or have gone through, the same transformation.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio268" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default Story;
