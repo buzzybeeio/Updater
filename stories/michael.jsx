6bba52d6389e5cd2698d84d7cf47131d3fd67317
Rai Lee <thisisrailee@gmail.com>
2017-12-11 10:45:04 -0800

    michael

---
+++ stories/michael.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Story = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/michael.jpg" alt="" />
+    <h4 className="story-title"><em>Michael, Engineer Consultant at Celerity Consulting Group</em></h4>
+    <p><em>This week we got a chance to interview MIchael Norelli. He is an Engineer Consultant at Celerity Consulting Group. Michael has attended General Assembly and is self-taught in programming. Here is the interview:</em></p>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I work at an engineering consulting firm in Walnut Creek. I’m continuing what I was doing before - working with geographic mapping systems. It doesn’t consist of much programming at the moment but we are moving toward integrating more software. I did get a chance to look into a project with my coworkers where we were writing code to make sure that there were no duplicate PDFs, but we didn’t end up using it since there weren’t many duplicate documents in our database.”</p>
+    <p><strong>How did you get started with programming?</strong></p>
+    <p>“In my work with the Golden Gate National Parks Conservancy, we were looking into corporate funding for nonprofits. We saw that Google had a grant program and we got in touch with them, which led to a partnership in hosting a large conference. Google came out and gave a talk to all the nonprofits present, and even offered to train many nonprofit employees, such as myself, on their latest Google map technology. I was introduced to ‘KML’ which is very similar to ‘XML’ and that ended up being the gateway to JavaScript. Python was used to write our geographic information software, so I taught myself Python. I also got involved with the Maptime meetup because it gets all the people together who want to learn about maps and programming. I felt like I needed to learn more, so I looked into coding programs and I came across General Assembly. In November 2015, I left the Parks Conservancy and decided to attend General Assembly. I started General Assembly in February 2016. It was a firehose of information, and I learned so much! It was amazing but I didn't follow up to start employment as a software engineer because I personally felt like I needed a little bit more practice. I also felt that at my age, it is a little different. I still aspire to be a software engineer.”</p>
+    <p><strong>What are you currently doing to become a software engineer?</strong></p>
+    <p>“I’m constantly learning and programming while I’m in transit, so I’m definitely using a lot of my downtime to make sure that everything that I’ve learned is being put to good use. I think that it’s a matter of convincing myself that I can do all the things that I’ve been taught. It’s working out pretty well.”</p>
+    <p><strong>What kind of projects are you looking to build?</strong></p>
+    <p>“I'm working on a custom map to show data from an online geographic database I've made.”</p>
+    <p><strong>What is some advice that you have for people who are learning programming?</strong></p>
+    <p>“When you come up with something and you’re stuck, the goal is to not stay stuck and be extremely creative about being unstuck. Google like crazy, call everyone you know, or even throw away that code and restart it - anything to get the creative juices flowing.”</p>
+    <p><strong>Can you tell us about some memorable lessons that you learned while coding?</strong></p>
+    <p>“I had moments at General Assembly where I was like, ‘Oh My God! There’s no way that I can get this done by the end of the week’, but as I learned and researched, I realized that I actually had a lot of methods that I could use and the task wasn’t so daunting anymore.”</p>
+    <p><strong>What did you enjoy about General Assembly?</strong></p>
+    <p>“The support was awesome and being around that kind of environment where everyone is there to learn is very enriching. I also like that they are always changing their curriculum so that the students always have access to the latest technology.”</p>
+    <p><strong>What are some challenges that you faced?</strong></p>
+    <p>“One challenge has been asking for help, because I have a hard time asking for help when I need to. I’m trying to remind myself to ask for help, because when someone teaches, it actually helps them fortify their understanding of the subject. So in a way, if you ask for help you are also helping the person who is helping you.”</p>
+    <p><strong>Any last thoughts/advice for people who are trying to become software engineers?</strong></p>
+    <p>“Be suspicious of your assumptions, especially about what your limits are.  Sometimes we don’t know at all what we are capable of until we try.  Life is an experiment - a learning exercise.  Sometimes ‘failure’ is just more data for the experiment.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default Story;
