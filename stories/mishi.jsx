f004443075e26f138bd808860b967738b1844091
Rai Lee <thisisrailee@gmail.com>
2017-12-27 12:00:31 -0800

    add Mishi's story

---
+++ stories/mishi.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Story = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/mishi.jpg" alt="" />
+    <h4 className="story-title"><em>Mishi, Software Engineer</em></h4>
+    <p><em>This week we got a chance to interview Mishi Charles. Mishi is on the journey toward being a Software Engineer; she is both self-taught and has attended a coding program, Learners Guild. Here is the interview:</em></p>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I grew up in the Bay Area - Oakland and Alameda. My introduction to programming actually happened way back in the mid-90s when I was in middle school. I remember messing with HyperTalk, DOS, and HTML during the dial-up days. It was fun but I was still set on working in a biology-based science or in music. I still need to keep both ‘sides’ of my brain engaged and programming seems to work for both.”</p>
+    <p><strong>What inspired you to become a software engineer?</strong></p>
+    <p>“Funny enough, it was actually stumbling into Codecademy.com while I was pregnant with my daughter. The latter parts of my pregnancy were difficult so I spent a lot of time on the computer to keep myself busy. I knew it was something I had a knack for and it was still fun. The final push came when a friend sent me a repo for a toy program because he was going to start at Dev Bootcamp.”</p>
+    <p><strong>What are the steps that you have taken to become a software engineer?</strong></p>
+    <p>“I've mostly done a lot of self-study. I eventually enrolled in a boot-camp-like program called Learners Guild which, while enriching, was not an ideal way for me to continue my software journey. The connections I made were priceless but it really didn't suit my learning style.”</p>
+    <p><strong>Where are you currently at in your journey to become a software engineer?</strong></p>
+    <p>“I'm in the job seeking phase. I'm working on improving my skills and getting my name on more projects that are in development. Mostly I've been networking a lot. Connections are extremely important for developers who come from non-traditional backgrounds. It's hard to get a foot in the door to demonstrate what you know when you don't know the right people.”</p>
+    <p><strong>Can you tell us about Learners Guild and your experience with it?</strong></p>
+    <p>“It was great to be in an environment full of people who were also enthusiastic about programming. Getting to geek out and have all of those brains to pick was an amazing experience. I don't think it's a good fit for many people or even most people but the appeal is understandable.”</p>
+    <p><strong>What are the most helpful resources to you on your journey of becoming a software engineer? </strong></p>
+    <p>“Udemy, Treehouse, and FreeCodeCamp are great resources for gaining knowledge. You can take a course on anything and just remember to take the time to actually apply that knowledge in practical ways.”</p>
+    <p><strong>What is some advice that you have for people who are trying to become software engineers?</strong></p>
+    <p>“You really have to enjoy the work if you want to be a software engineer. I love programming. It's equal parts solving puzzles and creating amazing things. If working until the wee hours of the morning on a problem because you're so inspired to solve it, if it doesn't sound like fun to you, this probably isn't the right field. Just get out there and start working. No project is too small and practice will never stop rewarding.”</p>
+    <p><strong>What are some obstacles that you’re encountering? What are some obstacles that you’ve overcome?</strong></p>
+    <p>“Imposter syndrome is the big one. There's so much to learn and that's a huge draw and a huge issue for me. I'm just trying to be more confident that I actually have more knowledge of my stack than I believe. I never thought I would take this risk. I guess fear of failure is an obstacle I have overcome. I now appreciate how valuable failure is to the learning process.”</p>
+    <p><strong>Any last thoughts/advice?  </strong></p>
+    <p>“Did I mention the benefits of practice?”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default Story;
