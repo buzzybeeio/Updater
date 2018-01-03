be4bf7df82dbfa0ebb6d316029b1fe387b1786d0
Rai Lee <thisisrailee@gmail.com>
2018-01-02 21:35:12 -0800

    add zeze

---
+++ stories/zeze.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Story = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/zeze.jpg" alt="" />
+    <h4 className="story-title"><em>José, Software Engineer at Inbenta</em></h4>
+    <p><em>This week we got a chance to interview José Manuel Vázquez Guízar. José is a Solutions Engineer at Inbenta. When he's not coding, he often times researches cryptocurrency. Here is the interview:</em></p>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I studied philosophy as an undergrad and went straight into a variety of sales and business development roles across different industries.”</p>
+    <p><strong>What made you want to become a Software Engineer?</strong></p>
+    <p>“I wanted to do something that wouldn’t get boring too quickly. What makes working with software exciting to me is that there is so much to learn all the time and things are in constant flux so that I can’t conceive getting bored.”</p>
+    <p><strong>Can you tell us about the obstacles that you had to overcome to get to where you are today? </strong></p>
+    <p>“Quitting a well paying job at 30 to start an apprenticeship program that was at its infancy stages was about the hardest thing to convince my family about. That, and the numerous interviews I went through, sure poked at my impostor’s syndrome, making me doubt myself many times.”</p>
+    <p><strong>What advice do you have for people who are looking to become software engineers?</strong></p>
+    <p>“I'm in the job seeking phase. I'm working on improving my skills and getting my name on more projects that are in development. Mostly I've been networking a lot. Connections are extremely important for developers who come from non-traditional backgrounds. It's hard to get a foot in the door to demonstrate what you know when you don't know the right people.“Always be learning and don’t give up on yourself when the going gets tough. It’s tough for all of us who come from nontraditional backgrounds, but these challenging moments make the victories so much sweeter. Keep some inspirational quotes nearby to get you through the rough times. ‘If you’re going through Hell, keep going!’ - Winston Churchill”</p>
+    <p><strong>If you could have done something differently to be where you are at, what would it be?</strong></p>
+    <p>“Make the career transition sooner! I had the chance of starting other bootcamps earlier, but I was too scared and lacked confidence to pull the trigger.”</p>
+    <p><strong>Did you have any mentors along the way? If so, how did they help you? If not, do you wish you had one?</strong></p>
+    <p>“Kinda. My mentors were very busy with their jobs and I did not really know how to be mentored at the time. Looking back at my experience, I could have been more proactive with how I shared questions/status updates about my job hunt with my mentors.”</p>
+    <p><strong>Can you tell us about your support group and how they kept you going through tough times?</strong></p>
+    <p>“I am fortunate to have family nearby and to have had a supportive partner who helped me stay positive and kept me motivated throughout the whole transition. They were always there to help me lift myself after every single rejection.”</p>
+    <p><strong>Are you currently working on any side projects? </strong></p>
+    <p>“Yes. I am redoing the dev work for a local band’s site, currently hosted with Godaddy and implemented using WordPress templates, to be more customized to their needs.”</p>
+    <p><strong>Any last thoughts/advice for people who are trying to become software engineers?</strong></p>
+    <p>“Always be learning and never be afraid to jump in the deep end. I think it is safe to say that it gets easier…or you just get better at tolerating not knowing. Either way - have fun, stay positive, never stop learning, and, sure as hell, never stop grinding! I sacrificed (and continue to sacrifice) many hours of sleep to get here. It ain’t easy, but it sure as hell ain’t impossible!”</p>
+    <p><strong>What are the best free resources for developers?</strong></p>
+    <p>“Eloquent JavaScript was my first FREE pdf where I learned the basics of JavaScript as a programming language and other basic programming concepts. FreeCodeCamp was the next step up where I learned to put together the CSS, HTML, and JavaScript. My other favorite FREE resources are W3Schools, MDN Web Docs, Google, and Stack Overflow.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default Story;
