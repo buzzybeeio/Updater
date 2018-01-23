cdfce6a64731ce6ef07f59f584c654943a0e96f4
Rai Lee <thisisrailee@gmail.com>
2018-01-22 17:33:58 -0800

    gorlan's story

---
+++ stories/gorlan.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const introducction = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/gorlan.jpg" alt="" />
+    <h4 className="story-title"><em>Gorlan, Software Engineer at Capital One</em></h4>
+    <p><em>This week we got a chance to interview Gorlan Yuen. Gorlan has over three year of experiences as a Software Engineer. Here is the interview:</em></p>
+  </div>
+);
+
+const interview = () => (
+  <div>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I was born and raised in Oakland. I went to elementary, middle, and high school there. After high school, I went to UC Berkeley, I studied Engineering Physics. It’s a major that encompasses upper division physics, and some general areas of engineering: civil, mechanical, electrical, and computer science. Towards the last two years, there were a lot of choices in electives.”</p>
+    <p><strong>How did that translate into coding?</strong></p>
+    <p>“During my third summer in college, as I was looking for internships, a lot of the opportunities were in computer science. Since they were looking for interns, they didn’t care that much about how much experience I had. In my first internship, I mostly worked in HTML, JavaScript, and CSS. So that’s how I got started.”</p>
+    <p><strong>After your first internship, was that when you realized that you wanted to be a Software Engineer?</strong></p>
+    <p>“I haven’t decided at that point, I thought that it would be an interesting career to explore. It was a pretty small company at the time, so it wasn’t a pure computer science internship because I worked with marketing and social media aspects of the business as well.”</p>
+    <p><strong>What are some challenges that you faced trying to land a full-time position?</strong></p>
+    <p>“Being a non-computer science graduate, my opportunities were much more limited. I had no idea how to go into interviews nor the experience, and I didn’t have the fundamentals.”</p>
+    <p><strong>How did you overcome those challenges?</strong></p>
+    <p>“I had a few friends in computer science and I would use a Subreddit forum, CS Career Questions, to prepare for interviews.”</p>
+    <p><strong>How did you land your first job?</strong></p>
+    <p>“I interviewed at a lot of places. The internships helped because it gave me some experience. My first job, I got it through uploading my resume at a job board. The ratio from application to interview to job offer is pretty low, and it decreases through each step.”</p>
+    <p><strong>I know that you are also currently looking for a new position, is the approach different?</strong></p>
+    <p>“I can be pickier now; I used to just shotgun everything, but I can look at what the company does now and have more options. Before it was, ‘Please give me a job’.”</p>
+    <p><strong>What is the salary range that you were looking for at the beginning of your career and what is the salary range that you are looking for after three years of experience?</strong></p>
+    <p>“In the beginning, my target salary was 70k and above, it was a big mistake. Don’t list your salary. Even for someone like me not having a CS background, it’s not a good idea to put your salary target. 70k was not my starting salary though, I was able to negotiate a higher salary. My second job, my target was 130k and above.”</p>
+    <p><strong>Can you tell us about what you did as a Software Engineer at McKesson your first job?</strong></p>
+    <p>“They are a health company. Some would say that they are a health tech company. The division that I was working with were building a data platform to help organizations collect and use the data. As you can imagine, there were a lot of client regulations so we were very careful.”</p>
+    <p><strong>Did you have enough experience for your job from your previous internships?</strong></p>
+    <p>“To be honest, not really. You learn on the job as you go. I think that it’s important to be humble as you go because there’s so much to learn and it’s extremely important to keep that in mind when you’re starting off.”</p>
+    <p><strong>How did you learn?</strong></p>
+    <p>“Yeah, so after my first job I picked up some Software Engineer books. There are skills that will help you get the job and there are other skills that will help you be a great Software Engineer. When I was first job searching, it was mostly interview prep but after getting the offer, I studied more on how to be a Software Engineer. ‘Cracking the Coding Interview’ and ‘Elements of Programming Interviews’ are more for interviewing. ‘Code Complete’ and ‘Design Patterns’ are for becoming better programmers.”</p>
+    <p><strong>Not having a CS degree, does that still make you feel the ‘Impostor Syndrome’?</strong></p>
+    <p>“Not so much now because I’ve been studying and working for three years now, but in the beginning I definitely did. Hearing people talk about certain things and being completely clueless to what they were saying definitely made me feel that way. But I would say to ask questions whenever you don’t understand something.”</p>
+    <p><strong>What are some advice that you have for people who are trying to become Software Engineers?</strong></p>
+    <p>“I guess that it depends on why you want to be a Software Engineer. If you’re doing it because there’s a lot of job opportunities, then pick up one of the interview books. If you’re looking to build something, then just start building and if you run into something, Google is there. There are endless resources.”</p>
+    <p><strong>What are some free resources that you come across? </strong></p>
+    <p>“Leetcode. It’s not so much building anything, but it’s what companies look at as an indicator of  a Software Engineer. If you’re able to solve those puzzles then you’re able to learn quickly. People can agree or disagree with that but it’s the current metric and everyone in Silicon Valley is doing it so might as well get good at coding those puzzles.”</p>
+    <p><strong>Any last thoughts/advise?</strong></p>
+    <p>“It takes a while to get ramped up especially if you don’t have exposure so just keep at it. If that’s something that you want, then don’t give up.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default { introducction, interview };
