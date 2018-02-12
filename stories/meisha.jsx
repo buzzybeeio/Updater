6062ba88a24a46b9a18760450294faf9877384ba
Rai Lee <thisisrailee@gmail.com>
2018-02-11 22:20:47 -0800

    Meisha's story

---
+++ stories/meisha.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const introducction = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/meisha.jpg" alt="" />
+    <h4 className="story-title"><em>Meisha, Customer Care/Technical Support at AugMedix</em></h4>
+    <p><em>This week we got a chance to interview Meisha Booker. Meisha is starting her career in Tech with Augmedix, she has worked extremely hard to be where she is currently at. Here is the interview:</em></p>
+  </div>
+);
+
+const interview = () => (
+  <div>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I am from Oakland, CA. My family moved to the Bay Area in the early 60’s from Louisiana. I have worked in technical support and customer care for the last 10 years with different companies such as Comcast, AT&T, etc. I have always loved technology and after a few years of working in different industries, I knew that my career was going to be doing something with tech. I switched from just wanting to do technical support to coding last year, in which I joined a bootcamp to help me progress my web development skills. I am an early stage web developer, still working on my web development skills building on the languages that I have learned.”</p>
+    <p><strong>What is your role at Augmedix?</strong></p>
+    <p>“Augmedix is a Google Glass company for health care providers across the United States. I am on the Customer Care team. My duties are to handle all technical issues the providers and scribes may encounter while using the glass units. We work closely with our Network and Software teams to help deliver a great service. As a web developer, I am hoping that in the future I will be working on one of those teams but for now I am enjoying learning my role and working with them as much as I can.”</p>
+    <p><strong>What obstacles did you have to overcome in order to achieve your goal of working in the Technology Field?</strong></p>
+    <p>“One of the major obstacles I had to overcome was the self doubt. I had been in the tech industry on the technical service side but it took somewhat of an internal push to go for it as a web developer. I thought I had missed my chance because I was ‘too old’ compared to some of the younger up-and-coming developers. Once I realized that I was not too old and not too far behind, I began to work hard at learning and building my web development skills.”</p>
+    <p><strong>What role did your family play on this journey?</strong></p>
+    <p>“I have two children, an eight year old boy and an eighteen month old girl. Balancing being a mom and trying to adjust my career in tech has its challenges. I am blessed to have a strong community of people to assist me, including my mother, the father of my children, my mother-in-law and close friends. My family gives me motivation to keep working hard and will not allow me to give up on my dreams.”</p>
+    <p><strong>What are some of the best resources that you’ve come across?</strong></p>
+    <p>“Udemy, Treehouse, and FreeCodeCamp are the resources that I currently use and have been using since the start of my web development career.”</p>
+    <p><strong>What are some advice for people who are trying to break into Tech and/or are trying to be Software Engineers?</strong></p>
+    <p>“Start as early as you possibly can either in high school, college, or freelance study. Use the resources that are free or low in cost online before trying to go to a bootcamp. And let me not forget... Network, network, network!”</p>
+    <p><strong>How did you stay positive?</strong></p>
+    <p>“Having a strong support system with family and friends along with the tenacity to keep pushing the limitations in the tech industry. I was raised in a city and community that has a historical background for social and economical change. I pride myself in breaking into an industry as a black woman knowing that I am a part of an underrepresented group.”</p>
+    <p><strong>If you could do it all over, is there anything that you would have done differently?</strong></p>
+    <p>“I would have started coding right out of high school or even while in high school. I was always a techy type of person from the beginning but was influenced to go in different directions. Once I noticed that technology was going to be the ‘Next Big Thing’ back in the early 2000’s, I should have jumped at the opportunity to start coding. I went to college for paralegal studies and before I graduated I was already looking for another career and working technical support.”</p>
+    <p><strong>Any last thoughts/insights that you would like to share with our readers?</strong></p>
+    <p>“It is never too late to try something new, so don’t be afraid to take that first step.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default { introducction, interview };
