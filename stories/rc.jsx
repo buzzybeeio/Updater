8700b16090149c008cc04b52eaee11f045c10381
Rai Lee <thisisrailee@gmail.com>
2018-02-20 12:24:12 -0800

    RC's story

---
+++ stories/rc.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const introducction = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/rc.jpg" alt="" />
+    <h4 className="story-title"><em>RC, Technical Teaching Fellow at Hack the Hood</em></h4>
+    <p><em>This week we got a chance to interview RC Brown. RC broke into the Tech Space and is passionate about empowering others through code. Here is the interview:</em></p>
+  </div>
+);
+
+const interview = () => (
+  <div>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I’ve been a DJ and Event Promoter since 2004. I spent 13 years throwing events for my community to provide a space of healing and release. In 2015, I decided that I wanted to find a career where I could blend all of my interests and make a future for myself. I wanted to shift into something that would make it possible for me to pay it forward to youth and provide me with stability in multiple areas of my life. I started teaching myself how to code, got a scholarship to Hack Reactors Fulcrum self-taught program, and from there I was accepted into an apprenticeship at Learners Guild in Oakland. After my time at the Guild, I was able to land a fantastic job at Hack the Hood.”</p>
+    <p><strong>What is your role at Hack the Hood? </strong></p>
+    <p>“I am a Technical Fellow at Hack the Hood. I am also the Lead Instructor during their Bootcamps (Spring and Summer) and I teach their Coding class. I get to develop curriculum, lead programming, and coordinate hackathons and other events.”</p>
+    <p><strong>What obstacles did you have to overcome to break into the Tech Field?</strong></p>
+    <p>“The biggest obstacle for myself has always been myself. I’m sure most self-taught engineers would say something similar. I could also mention that before my apprenticeship, one of the bigger obstacles I experienced was finances. I got into many different coding bootcamps, but couldn’t afford the upfront costs. I often wonder what the tech industry would look like if it were more accessible in that respect.”</p>
+    <p><strong>Prior to Hack the Hood, what was your job search process?</strong></p>
+    <p>“I became a pro at applying to jobs via Angelist, LinkedIn, and many other tech job search engines. I had the most luck applying for jobs where a friend could recommend me. If I didn’t have a friend in the company, I would find someone on LinkedIn and introduce myself. I spent 13 years being social for a living, I have no shame in hitting up someone I don’t know for a coffee and chat.”</p>
+    <p><strong>What are some of the best resources that you’ve come across?</strong></p>
+    <p>“The best resource I found was YouTube. I mean, you can find a video of anything and everything on there. You have to dig through them and find the best video tutorials like when you are digging in record crates or going down rabbit holes on SoundCloud.”</p>
+    <p><strong>What are some advice for people who are trying to break into Tech and/or are trying to be Software Engineers?</strong></p>
+    <p>“Whatever you do, always stay up-to-date on the basics. They go a long way and help to solidify complex concepts. They’re your foundation, keep it sturdy!”</p>
+    <p><strong>What is the easiest and hardest interview question that you remember?</strong></p>
+    <p>“Write a recursive function for traversing a binary search tree. It was pretty hard for me; I sweated bullets the whole way through. But I thought of how a node works and it helped me work it out. I don’t think I’ve ever been asked an ‘easy’ question during an interview, which I guess I am lucky for.”</p>
+    <p><strong>If you could do it all over, is there anything that you would have done differently?</strong></p>
+    <p>“I wouldn’t do it any differently at all. I love how becoming a software engineer has changed my trajectory in life and I’m excited to see where it takes me.”</p>
+    <p><strong>Any last thoughts/insights that you would like to share with our readers?</strong></p>
+    <p>“The world of tech is vast, if you have any interest in it, you have to go for it. It’ll be hard, but nothing worth the trouble is easy.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default { introducction, interview };
