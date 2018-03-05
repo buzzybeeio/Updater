72ec856582cf7b62ad03f8a8f946253ba181ef81
Rai Lee <thisisrailee@gmail.com>
2018-03-05 09:54:48 -0800

    kavin's story

---
+++ stories/kavin.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const introducction = () => (
+  <div>
+    <img className="narrator-pic" src="profilepics/kavin.jpg" alt="" />
+    <h4 className="story-title"><em>Kavin, Software Engineering Student at 42 University</em></h4>
+    <p><em>This week we got a chance to interview Kavin. Kavin is pursuing his dream of becoming a Software Engineer through attending 42 University. Here is the interview:</em></p>
+  </div>
+);
+
+const interview = () => (
+  <div>
+    <p><strong>Can you tell us a little bit about your background?</strong></p>
+    <p>“I am a first generation immigrant that grew up in the Bay Area. After having spent that many years in one location, I moved down south for two years to finish college in San Diego, where I completed an undergraduate degree in International Business.”</p>
+    <p><strong>Why are you interested in coding?</strong></p>
+    <p>“I’ve always been enamored with entrepreneurship and technology. I remember my first trip to San Francisco, where I encountered a large Yahoo billboard. When I got home, I went on Yahoo’s website and spent a good hour just exploring the website.</p>
+    <p>Finishing college, I’ve always wanted to explore the entrepreneurship route and see if I can at least make it in that world. I have had ideas that I wanted to start, but just didn’t know how to get it started. Also, a stint working with startups reaffirmed my decision to go into entrepreneurship.</p>
+    <p>These dilemmas led me to find a Udacity course, in which I first learned how to code. I was quickly interested and enamored in the topic of coding.”</p>
+    <p><strong>What obstacles are you going through on your way of becoming a Software Engineer?</strong></p>
+    <p>“Not going to lie here: learning how to code is definitely a challenge.</p>
+    <p>First of all, I’m horrible at thinking abstractly. Also, I would be able to see the big picture when trying to solve problems, but would be horrible at trying to break the big picture down into smaller, more solvable problems/solutions.</p>
+    <p>Second, I suffer from a condition called Tourette’s Syndrome, a nasty neurological disease which unfortunately doesn’t let me concentrate on tasks at hand for very long.”</p>
+    <p><strong>Can you tell us about 42 university?</strong></p>
+    <p>“42 is a free coding school in which it challenges and pushes you to do your absolute best.</p>
+    <p>It started out in France, where are a billionaire by the name of Xavier Niel started this school in hopes of churning out more software engineers and entrepreneurs to help contribute to the French economy. There are no teachers, so in order to learn, you must utilize every resource available(the internet/your peers).</p>
+    <p>To get admitted into the school, you must undergo two online logic tests and a one month intensive bootcamp called the ‘piscine’. During the piscine, you will be coding in C for 10-15 hrs everyday. It will be mentally and physically draining, but you’ll learn lots and make lifelong friends. If you’re successful, you’ll be admitted into 42 as a student.”</p>
+    <p><strong>What projects have you worked on at 42 University and how have they helped you?</strong></p>
+    <p>“The first two projects I’ve worked on are recreating a C library that includes important functions and an assignment that aims to recreate the basic functionalities of Tetris.</p>
+    <p>I’ve also been supplementing 42’s assignments with other online courses and recreating basic, but functional web pages, which a friend and I have done thanks to the web development club at 42.</p>
+    <p>All these assignments have taught me many things, but the most important two being: thinking abstractly and learning how to solve problems by breaking it down into its smallest, minute details, and to then recreate it.”</p>
+    <p><strong>Would you recommend 42 University?</strong></p>
+    <p>“If you’re not afraid to fail and retry and have the desire to better yourself, then I would wholeheartedly recommend 42. Besides, it’s the only free coding school (without conditions) here in the Bay Area.”</p>
+    <p><strong>What are some low points on your way to becoming a Software Engineer?</strong></p>
+    <p>“One of the low point in my path to becoming a software engineer is that I didn’t think I would a be a good one, let alone a decent SWE. For the online coding videos I obtained, I would do well in the early parts of the course, only to stumble when the material gets more challenging. After receiving absolutely no help from the supposedly friendly forums, I’d eventually give up. This happened many times for a lot of the courses that I took related to computer science. I honestly thought I was incapable of learning the material that many have mastered.”</p>
+    <p><strong>How do you keep yourself motivated?</strong></p>
+    <p>“I like to keep myself motivated by completing mini tasks while I’m coding: by completing and creating short but manageable web pages and by finishing small-sized bits of work, overall, in a big project.</p>
+    <p>While I’m not coding, I like to read a good book, enjoy coffee, and meet-up/socialize with close friends.”</p>
+    <p><strong>Any last thoughts/ advice for our readers?</strong></p>
+    <p>“Stay hungry, stay curious, and most of all, never give up in what you are trying to pursue.”</p>
+    <p><em>For more awesome stories check out our <a href="https://www.facebook.com/buzzybee.io/" target="_blank" rel="noopener noreferrer">facebook</a> and <a href="https://medium.com/@buzzybeeio" target="_blank" rel="noopener noreferrer">medium</a> page!</em></p>
+  </div>
+);
+
+export default { introducction, interview };
